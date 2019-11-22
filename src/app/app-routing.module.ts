333c48cbdaf6f1cdcd6ad9401ae3353ed4afef42
bruno <brunohma@live.com>
2019-11-22 20:39:13 -0300

    exercicio 23

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ListaCartaoComponent } from './lista-cartao/lista-cartao.component';
+import { NovoCartaoComponent } from './novo-cartao/novo-cartao.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'listagem', component: ListaCartaoComponent},
+  {path: 'novo', component: NovoCartaoComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
