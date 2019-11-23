e90b6d76de97bfff998ee7519c1862525fbf2e4e
bruno <brunohma@live.com>
2019-11-22 22:01:00 -0300

    exercicio 26 corrigido

--- src/app/cartao.ts
+++ src/app/cartao.ts
@@ -1,2 +1,8 @@
 export class Cartao {
-}
+
+    public numero_cartao : string;
+    public nome_cartao : string;
+    public data_vencimento : number;
+    public codigo_verificacao : number;
+  
+  }
